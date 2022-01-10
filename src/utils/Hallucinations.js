32875131309c393fccc44db1d5fc4f2b4d88a9df
Matthias Holcombe <tmatthiash@gmail.com>
2022-01-09 20:26:52 -0600

    increase hallucination difficulty

--- src/utils/Hallucinations.js
+++ src/utils/Hallucinations.js
@@ -4,7 +4,7 @@ export const generateHallucinations = () => {
     console.log("generate")
     const allCells = document.getElementsByClassName("game-grid-cell")
     for(let i = 0; i < allCells.length; i++) {
-        if(randomInt(12) ===0){
+        if(randomInt(10) ===0){
             allCells[i].classList.add("hallucination");
         }
     }
@@ -12,7 +12,6 @@ export const generateHallucinations = () => {
 
 export const clearHallucinations = () => {
     const allHallucinationCells = document.getElementsByClassName("game-grid-cell");
-    console.log("clearing", allHallucinationCells.length)
     for(let j = 0; j < allHallucinationCells.length; j++) {
         allHallucinationCells[j].classList.remove("hallucination");
     }
